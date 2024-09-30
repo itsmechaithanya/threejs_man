d888f4e42e9c0e4c8a36d8d55e698898e5d59857
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-09-30 20:27:06 +0530

    first commit

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100 );
 camera.position.z = 5;
+scene.add(camera);
 
-const canvas = document.querySelector("#draw");
-const renderer = new THREE.WebGLRenderer({canvas});
-renderer.setSize( window.innerWidth, window.innerHeight );
+let box = new THREE.BoxGeometry(1,1,1);
+let material = new THREE.MeshBasicMaterial({ color: "red" });
+let mesh = new THREE.Mesh (box, material);
 
-function animate() {
-    window.requestAnimationFrame( animate );
-	renderer.render( scene, camera );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+// mesh.position.x = 1;
+// mesh.position.y = 1;
+// mesh.position.z = 1;
+
+// mesh.rotation.x = 1;
+// mesh.rotation.y = 1;
+// mesh.rotation.z = 1;
+
+// mesh.scale.x = 2;
+// mesh.scale.y = 1;
+// mesh.scale.z = 1;
+
+mesh.rotation.y = Math.PI / 4
+mesh.rotation.x = Math.PI / 4
+
+scene.add(mesh);
+
+const canvas = document.querySelector ('canvas');
+let renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window. innerWidth, window. innerHeight);
+renderer.render(scene, camera);
+
+function animate(){
+    window.requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    mesh.rotation.y += 0.01;
 }
 animate();
-
