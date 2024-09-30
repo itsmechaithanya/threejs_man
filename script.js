a6eaabeacbfc6df0c1e4b357d16466308f0daf1d
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-09-30 20:58:03 +0530

    playing with cdn

--- script.js
+++ script.js
@@ -25,13 +25,14 @@ mesh.rotation.x = Math.PI / 4
 scene.add(mesh);
 
 const canvas = document.querySelector ('canvas');
-let renderer = new THREE.WebGLRenderer({ canvas });
+let renderer = new THREE.WebGLRenderer({ canvas: canvas , antialias: true });
 renderer.setSize(window. innerWidth, window. innerHeight);
 renderer.render(scene, camera);
 
+let clock = new THREE.Clock();
 function animate(){
     window.requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    mesh.rotation.y += 0.01;
+    mesh.rotation.y = clock.getElapsedTime()  ;
 }
 animate();
